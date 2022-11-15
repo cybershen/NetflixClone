e918ae97fc282bbe2161fad685e293d8a44dd072
cybershen <cyberpuer@proton.me>
2022-11-15 11:39:45 +0200

    Create TopSearch TableView inside TopSearch tab

--- NetflixClone/Managers/APICaller.swift
+++ NetflixClone/Managers/APICaller.swift
@@ -106,4 +106,21 @@ class APICaller {
         }
         task.resume()
     }
+    
+    func getDiscoverMovies(completion: @escaping (Result<[Title], Error>) -> Void) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/discover/movie?api_key=\(Constants.API_KEY)&language=en-US&page=1") else { return }
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
+                completion(.success(results.results))
+            } catch {
+                completion(.failure(APIError.failedToGetData))
+            }
+        }
+        task.resume()
+    }
 }
