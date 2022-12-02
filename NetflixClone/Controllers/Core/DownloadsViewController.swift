45e88c5fbd677b8c6833f6cc3f2f12ad0ac0ddd9
cybershen <cyberpuer@proton.me>
2022-12-02 21:16:16 +0200

    Use Notification Center to update View Controllers

--- NetflixClone/Controllers/Core/DownloadsViewController.swift
+++ NetflixClone/Controllers/Core/DownloadsViewController.swift
@@ -23,9 +23,14 @@ class DownloadsViewController: UIViewController {
         title = "Downloads"
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.tintColor = .white
+        
         downloadedTable.delegate = self
         downloadedTable.dataSource = self
         fetchLocalStorageForDownload()
+        NotificationCenter.default.addObserver(forName: NSNotification.Name("downloaded"), object: nil, queue: nil) { _ in
+            self.fetchLocalStorageForDownload()
+        }
     }
     
     private func fetchLocalStorageForDownload() {
@@ -82,6 +87,30 @@ extension DownloadsViewController: UITableViewDelegate, UITableViewDataSource {
             }
         default:
             break
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let title = titles[indexPath.row]
+        
+        guard let titleName = title.original_title ?? title.original_name else {
+            return
+        }
+        
+        APICaller.shared.getMovie(with: titleName) { [weak self] result in
+            switch result {
+            case .success(let videoElement):
+                DispatchQueue.main.async {
+                    let vc = TitlePreviewViewController()
+                    vc.configure(with: TitlePreviewViewModel(title: titleName, youtubeView: videoElement, titleOverview: title.overview ?? ""))
+                    self?.navigationController?.pushViewController(vc, animated: true)
+                }
+                
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
         }
     }
 }
--- NetflixClone/Views/CollectionViewTableViewCell.swift
+++ NetflixClone/Views/CollectionViewTableViewCell.swift
@@ -55,7 +55,7 @@ class CollectionViewTableViewCell: UITableViewCell {
         DataPersistenseManager.shared.downloadTitleWith(model: titles[indexPath.row]) { result in
             switch result {
             case .success():
-                print("Downloaded to Database")
+                NotificationCenter.default.post(name: NSNotification.Name("downloaded"), object: nil)
             case .failure(let error):
                 print(error.localizedDescription)
             }
